e5694646146f3083d45fb6c446fbc96650a93e78
Rishi Vishwakarma <anotherishi@tuta.io>
2025-01-31 18:29:15 +0530

    formatting

--- src/components/Signin.jsx
+++ src/components/Signin.jsx
@@ -1,79 +1,107 @@
 /* eslint-disable no-unused-vars */
 // import React from "react";
 import { useState } from "react";
-import "./Signin.css"
+import "./Signin.css";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import axios from "axios"
+import axios from "axios";
 
 const Signin = () => {
-  const [formData, setFormData] = useState({
-    username: "",
-    email: "",
-    password: "",
-  });
-  const [loading, setLoading] = useState(false);
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData((prev) => ({ ...prev, [name]: value }));
-  };
+    const [formData, setFormData] = useState({
+        username: "",
+        email: "",
+        password: "",
+    });
+    const [loading, setLoading] = useState(false);
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData((prev) => ({ ...prev, [name]: value }));
+    };
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    if (!formData.username || !formData.email || !formData.password) {
-      toast.error("All fields are required");
-      return;
-    }
-    setLoading(true);
-    try {
-      const response=await axios.post(`http://localhost:8000/users/register`, formData);
-      // console.log(response);
-      toast.success("Form has been submitted");
-    } catch (error) {
-      if(error.status==409){
-        toast.error("User already exists");
-        return;
-      }
-      toast.error("Error in submitting form");
-    }
-    setLoading(false);
-  };
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        if (!formData.username || !formData.email || !formData.password) {
+            toast.error("All fields are required");
+            return;
+        }
+        setLoading(true);
+        try {
+            const response = await axios.post(
+                `http://localhost:8000/users/register`,
+                formData
+            );
+            // console.log(response);
+            toast.success("Form has been submitted");
+        } catch (error) {
+            if (error.status == 409) {
+                toast.error("User already exists");
+                return;
+            }
+            toast.error("Error in submitting form");
+        }
+        setLoading(false);
+    };
 
-  return (
-    <div className="signin-body">
-      <div className="signin-cont">
-
-      <ToastContainer 
-        position="top-center"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable={false}
-        pauseOnHover={false}
-        theme="light"
-      />
-        <h1>Sign Up</h1>
-        <form onSubmit={handleSubmit}>
-            <div className="signin-form-div">
-                <div className="signin-username-label">Username: </div>
-            <input type="text" name="username" placeholder="username" autoComplete="off" required onChange={handleChange} value={formData.username}/>
+    return (
+        <div className="signin-body">
+            <div className="signin-cont">
+                <ToastContainer
+                    position="top-center"
+                    autoClose={5000}
+                    hideProgressBar={false}
+                    newestOnTop={false}
+                    closeOnClick
+                    rtl={false}
+                    pauseOnFocusLoss
+                    draggable={false}
+                    pauseOnHover={false}
+                    theme="light"
+                />
+                <h1>Sign Up</h1>
+                <form onSubmit={handleSubmit}>
+                    <div className="signin-form-div">
+                        <div className="signin-username-label">Username: </div>
+                        <input
+                            type="text"
+                            name="username"
+                            placeholder="username"
+                            autoComplete="off"
+                            required
+                            onChange={handleChange}
+                            value={formData.username}
+                        />
+                    </div>
+                    <div className="signin-form-div">
+                        <div className="signin-email-label">Email: </div>
+                        <input
+                            type="email"
+                            name="email"
+                            placeholder="email"
+                            autoComplete="off"
+                            required
+                            onChange={handleChange}
+                            value={formData.email}
+                        />
+                    </div>
+                    <div className="signin-form-div">
+                        <div className="signin-password-label">Password: </div>
+                        <input
+                            type="password"
+                            name="password"
+                            placeholder="password"
+                            autoComplete="off"
+                            required
+                            onChange={handleChange}
+                            value={formData.password}
+                        />
+                    </div>
+                    <button className="signinformsubmit" type="submit">
+                        Submit
+                    </button>
+                </form>
             </div>
-            <div className="signin-form-div">
-                <div className="signin-email-label">Email: </div>
-            <input type="email" name="email" placeholder="email" autoComplete="off" required onChange={handleChange} value={formData.email}/>
-            </div>
-            <div className="signin-form-div">
-                <div className="signin-password-label">Password: </div>
-            <input type="password" name="password" placeholder="password" autoComplete="off" required onChange={handleChange} value={formData.password}/>
-            </div>
-            <button className="signinformsubmit" type="submit">Submit</button>
-        </form>
-      </div>
-    </div>
-  )
-}
+        </div>
+    );
+};
 
-export default Signin
+export default Signin;
--- src/contexts/authContext.jsx
+++ src/contexts/authContext.jsx
@@ -1,52 +1,57 @@
 /* eslint-disable react-refresh/only-export-components */
 /* eslint-disable react/prop-types */
-import { createContext, useState, useEffect } from 'react';
-import { useContext } from 'react';
-import { Navigate } from 'react-router';
-import axios from 'axios';
-import { useLocation } from 'react-router';
+import { createContext, useState, useEffect } from "react";
+import { useContext } from "react";
+import { Navigate } from "react-router";
+import axios from "axios";
+import { useLocation } from "react-router";
 const AuthContext = createContext();
-  
+
 export const AuthProvider = ({ children }) => {
     const [isAuthenticated, setIsAuthenticated] = useState(false);
-    const [user, setUser] = useState(null); 
+    const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true);
     const location = useLocation();
     useEffect(() => {
         const checkAuth = async () => {
-            const accessToken = localStorage.getItem('accessToken');
+            const accessToken = localStorage.getItem("accessToken");
             console.log(accessToken);
             if (accessToken) {
                 try {
-                    const response = await axios.get('/users/current-user'); 
+                    const response = await axios.get("/users/current-user");
                     if (response.status === 200) {
-                      setIsAuthenticated(true);
-                      setUser(response.data.data.user); 
-                      //setuser properly
-                      console.log("HI user: ",response);
-                      return <Navigate to="/" replace />; // Redirect to login
-                    } else {
-                      localStorage.removeItem('accessToken');
-                    }
-                  } catch (error) {
-                    console.log("Get current user error: ",error);
-                    //401-token hai but expired hai refreshToken call laga
-                    if(error.status==401){
-                      try {
-                        console.log("Trying to refresh token");
-                        const response = await axios.post('/users/refresh-token'); 
-                        console.log("Refreshed Access Token: ",response);
-                        //store accessToken in localStorage here
-                        localStorage.setItem('accessToken', response.data.data.accessToken);
+                        setIsAuthenticated(true);
+                        setUser(response.data.data.user);
+                        //setuser properly
+                        console.log("HI user: ", response);
                         return <Navigate to="/" replace />; // Redirect to login
-                    } catch (error) {
-                      //agar refresh token pe error ara hai toh login pe bhej
-                      console.log("Error in refresh Token: ",error);
-                      setIsAuthenticated(false);
-                      return <Navigate to="/login" replace />;
+                    } else {
+                        localStorage.removeItem("accessToken");
                     }
-                  }
-                    localStorage.removeItem('accessToken');
+                } catch (error) {
+                    console.log("Get current user error: ", error);
+                    //401-token hai but expired hai refreshToken call laga
+                    if (error.status == 401) {
+                        try {
+                            console.log("Trying to refresh token");
+                            const response = await axios.post(
+                                "/users/refresh-token"
+                            );
+                            console.log("Refreshed Access Token: ", response);
+                            //store accessToken in localStorage here
+                            localStorage.setItem(
+                                "accessToken",
+                                response.data.data.accessToken
+                            );
+                            return <Navigate to="/" replace />; // Redirect to login
+                        } catch (error) {
+                            //agar refresh token pe error ara hai toh login pe bhej
+                            console.log("Error in refresh Token: ", error);
+                            setIsAuthenticated(false);
+                            return <Navigate to="/login" replace />;
+                        }
+                    }
+                    localStorage.removeItem("accessToken");
                 }
             }
             setLoading(false);
@@ -55,19 +60,21 @@ export const AuthProvider = ({ children }) => {
     }, [location]);
 
     const login = async (accessToken, user) => {
-        localStorage.setItem('accessToken', accessToken);
+        localStorage.setItem("accessToken", accessToken);
         setIsAuthenticated(true);
         setUser(user);
     };
 
     const logout = () => {
-        localStorage.removeItem('accessToken');
+        localStorage.removeItem("accessToken");
         setIsAuthenticated(false);
         setUser(null);
     };
 
     return (
-        <AuthContext.Provider value={{ isAuthenticated, user, login, logout, loading }}>
+        <AuthContext.Provider
+            value={{ isAuthenticated, user, login, logout, loading }}
+        >
             {children}
         </AuthContext.Provider>
     );
